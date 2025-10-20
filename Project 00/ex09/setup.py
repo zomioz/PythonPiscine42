fb4f09c91923ea2ee43e46f84f5331c72b79cece
zomioz <pierrerulence@gmail.com>
2025-10-20 16:01:49 +0200

    ex09 done

---
+++ Project 00/ex09/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    description='A small package from Python Piscine 42',
+    url='https://github.com/zomioz/PythonPiscine42/tree/main/Project%2000',
+    author='pirulenc',
+    author_email='pirulenc@student.42angouleme.fr',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+)
