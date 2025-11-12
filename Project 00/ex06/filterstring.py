823b9849ca8b92aab19f578e34781f27752915ae
zomioz <pierrerulence@gmail.com>
2025-11-12 09:24:42 +0100

    clean filtering.py

--- Project 00/ex06/filterstring.py
+++ Project 00/ex06/filterstring.py
@@ -4,41 +4,43 @@ from ft_filter import ft_filter
 
 def checker_arg():
 
-    '''
-    Checker of arg, verify number and type of arg
-    no arguments
-    return 1 on fail
-    '''
+  '''
+  function to check argument of the program
+  no argument
+  return 1 on Fail, 0 on succes
+  '''
 
-    number_of_arg = len(sys.argv) - 1
+  number_of_arg = len(sys.argv) - 1
 
-    try:
-        if number_of_arg != 2:
-            raise AssertionError("Wrong number of Argument")
-        try:
-            int(sys.argv[2])
-        except ValueError:
-            raise AssertionError("Second Argument isn't an integer")
-
-    except AssertionError as Error:
-        print("AssertionError :", Error)
-        return 1
+  try:
+      if number_of_arg != 2:
+          raise AssertionError("the arguments are bad")
+      try:
+          number = int(sys.argv[2])
+      except ValueError:
+          raise AssertionError("the arguments are bad")
+  except AssertionError as Error:
+      print("AssertionError:", Error)
+      return 1
+  return 0
 
 
 def main():
 
     '''
-    main of the program, launch checker and ft_filter
-    no argument
+    main of the program, launch checker and printf ft_filter output
+    no arguments
     no return
     '''
 
     if checker_arg() == 1:
         exit()
 
-    N = int(sys.argv[2])
-    words = sys.argv[1].split()
-    result = ft_filter(lambda word: len(word) > N, words)
+    tmp = sys.argv[1]
+    number = int(sys.argv[2])
+    words = string.split()
+
+    result = ft_filter(lambda word: len(word) > number, words)
 
     print(result)
 
